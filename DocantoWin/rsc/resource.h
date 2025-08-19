63a21158a22970a60663362bfa66407ac9a5846e
Gamanius <janluca.binder@hotmail.de>
2025-08-19 21:16:21 +0200

    added new temporary icon

--- DocantoWin/rsc/resource.h
+++ DocantoWin/rsc/resource.h
@@ -6,17 +6,17 @@
 #define IDR_SVG_MOVE_TOOL               102
 #define IDR_SVG_ERASE_TOOL              103
 #define IDR_SVG_SELECT_TOOL             104
-
 #define IDR_SVG_SAVE_SYMBOL             105
-
-#define IDC_CURSOR_GRAB                  201
-#define IDC_CURSOR_GRABBING              202
+#define IDI_ICON1                       107
+#define IDI_ICON_1                      107
+#define IDC_CURSOR_GRAB                 201
+#define IDC_CURSOR_GRABBING             202
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_RESOURCE_VALUE        108
 #define _APS_NEXT_COMMAND_VALUE         40002
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           102
